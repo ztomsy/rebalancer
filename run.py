f37ea1fd27d013e925337c8f9a4fd14cb787fff6
Nekrasovp <nekrasovp@gmail.com>
2019-05-30 17:16:15 +0300

    redesign setting watcher processing

--- run.py
+++ run.py
@@ -3,26 +3,26 @@
 import time
 import sys
 
-import _settings
+from yat.settingsWatcher import settingsWatcher
 from payload.runner import Runner
 from yat import logger
 
 
-# Parse settings file for
-settings = {attr: getattr(_settings, attr) for attr in dir(_settings) if not callable(getattr(_settings, attr)) and
-            not attr.startswith("__")}
+# Parse settings file into dict
+watcher = settingsWatcher()
+settings = watcher.settings
 
 # Init logger and define log_level verbosity
 logger = logger.setup_custom_logger('Rebalancer', log_level=settings['LOG_LEVEL'])
+
 
 if __name__ == '__main__':
     start = time.time()
     # Try/except just keeps ctrl-c from printing an ugly stacktrace
     try:
-        market1 = Runner(logger=logger, **settings)
-    except (KeyboardInterrupt, SystemExit):
-        logger.info('Rebalancer stop working after %.2f minutes' % ((time.time() - start) / 60))
+        market1 = Runner(logger=logger, watcher=watcher, **settings)
     except Exception:
         logger.error("Stop main loop for next reason:", exc_info=True)
-
+    except (KeyboardInterrupt, SystemExit):
+        logger.info('Rebalancer stop working after %.2f minutes' % ((time.time() - start) / 60))
     sys.exit()
---
+++ yat/settingsWatcher.py
@@ -0,0 +1,49 @@
+import os
+from importlib import reload
+import _settings
+
+
+class settingsWatcher(object):
+
+    # Constructor
+    def __init__(self, watch_file: str = '_settings.py'):
+        self._cached_stamp = 0
+        self.settings = {}
+        self.settings = self.build_dict()
+        self.filename = watch_file
+        self.call_func_on_change = self.reload_watched_file
+
+    # Look for changes
+    def look(self):
+        is_refreshed = False
+        stamp = os.stat(self.filename).st_mtime
+        if stamp != self._cached_stamp:
+            self._cached_stamp = stamp
+            # File has changed, call reloading module
+            is_refreshed = True
+            if self.call_func_on_change is not None:
+                self.call_func_on_change()
+        return is_refreshed
+
+    # Call this function each time a change happens
+    def reload_watched_file(self):
+        self.importOrReload('_settings')
+        self.settings.clear()
+        self.settings = self.build_dict()
+
+    @staticmethod
+    def importOrReload(module_name, *names):
+        import sys
+
+        if module_name in sys.modules:
+            reload(sys.modules[module_name])
+        else:
+            __import__(module_name, fromlist=names)
+
+        for name in names:
+            globals()[name] = getattr(sys.modules[module_name], name)
+
+    # Build and return a dict of settings from imported module
+    def build_dict(self):
+        return {attr: getattr(_settings, attr) for attr in dir(_settings) if not callable(
+                getattr(_settings, attr)) and not attr.startswith("__")}
