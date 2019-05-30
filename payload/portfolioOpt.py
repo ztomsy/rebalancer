7bf11967fc67b268987233d1aa7fb8d15601b9ba
Nekrasovp <nekrasovp@gmail.com>
2019-05-30 17:15:18 +0300

    new build_pricing_data_from_ohlcv version

--- payload/portfolioOpt.py
+++ payload/portfolioOpt.py
@@ -1,4 +1,3 @@
-import math
 import datetime
 from time import ctime
 from copy import deepcopy
@@ -28,10 +27,9 @@ class PortfolioOpt:
         return elapsed
 
     @ staticmethod
-    def as_date(date_string):
+    def as_days(date_string):
         if date_string is None: return None
         try:
-            # TODO make it available for different time format
             return datetime.datetime.strptime(date_string, '%Y-%m-%d')
         except ValueError:
             return None
@@ -52,25 +50,18 @@ class PortfolioOpt:
         except ValueError:
             return None
 
-    @ staticmethod
-    def months_from_date(start_date, months_offset):
-        # TODO make it available for different time format
-        year, month, day = start_date.timetuple()[:3]
-        new_month = month + months_offset
-        return datetime.date(year + math.floor(new_month / 12), (new_month % 12) or 12, day)
-
     def progress(self, msg):
         return "({:.3f} sec) {}...".format(self.elapsed_time(), msg)
 
     # endregion
 
     # region Optimizer
-    def generate_analysis_model(self, pricing_data, weight_bounds, frequency):
+    def generate_analysis_model(self, pricing_data, weight_bounds, frequency, gamma):
         mu = expected_returns.mean_historical_return(pricing_data, frequency=frequency)
         s = risk_models.sample_cov(pricing_data, frequency=frequency)
         range_begin = pricing_data.index[0]
         range_end = pricing_data.index[-1]
-        return range_begin, range_end, EfficientFrontier(mu, s, weight_bounds=weight_bounds, gamma=1)
+        return range_begin, range_end, EfficientFrontier(mu, s, weight_bounds=weight_bounds, gamma=gamma)
 
     def forward_looking_return(self, pricing_data, start_date, end_date, weights):
         df_pricing = pricing_data \
@@ -87,22 +78,30 @@ class PortfolioOpt:
 
         return range_begin, range_end, wavg_return
 
-    def build_pricing_data_from_ohlcv(self, portfolio_ohlcv: dict, assets_list: list):
+    def build_pricing_data_from_ohlcv(self, portfolio_ohlcv: dict, base_asset: str):
         """
         >>> pricing_data =  {'BTC/USDT': [[1258317600000, 7965.08, 7966.78, 7830.0, 7889.32, 1755.837013],[1258321200000, 7889.33, 8067.3, 7885.85, 8030.04, 2082.664391]],'ETH/USDT': [[1258317600000, 250.07, 250.08, 246.0, 247.57, 26873.12446],[1258321200000, 247.6, 254.65, 247.27, 254.05, 27160.78046]]}
         >>> po = PortfolioOpt()
-        >>> po.build_pricing_data_from_ohlcv(pricing_data).info()
+        >>> po.build_pricing_data_from_ohlcv(pricing_data, 'USDT').info()
         <class 'pandas.core.frame.DataFrame'>
         Index: 2 entries, Sun Nov 15 23:40:00 2009 to Mon Nov 16 00:40:00 2009
-        Data columns (total 2 columns):
-        BTC/USDT    2 non-null float64
-        ETH/USDT    2 non-null float64
-        dtypes: float64(2)
-        memory usage: 48.0+ bytes
+        Data columns (total 3 columns):
+        (BTC,)     2 non-null float64
+        (ETH,)     2 non-null float64
+        (USDT,)    2 non-null int64
+        dtypes: float64(2), int64(1)
+        memory usage: 64.0+ bytes
 
         :param portfolio_ohlcv:
         :return:
         """
+        # Convert names
+        # FIXME Check for assets_list and markets proper name
+        assets_list = []
+        for m in portfolio_ohlcv.keys():
+            asset = m[0:m.index('/')]
+            assets_list.append(asset)
+
         # assets_list = [x for x in portfolio_ohlcv.keys()]
         data_c = pd.DataFrame()
         for i, j in portfolio_ohlcv.items():
@@ -115,26 +114,39 @@ class PortfolioOpt:
             data["date"] = data["date"].apply(lambda x: ctime(x / 1000.0))
             data = data.set_index("date")
             data_c[i] = data['c']
-        # FIXME Check for assets_list and markets proper name
 
         data_c.columns = [assets_list]
+        # Add new column with 1 price for base asset
+        data_c[base_asset] = 1
         return data_c
 
-    def generate_report(self, pricing_data, weight_bounds, assets_list, frequency: int):
-        p_d = deepcopy(self.build_pricing_data_from_ohlcv(pricing_data, assets_list))
-        # TODO Add separate bounds for each asset <class 'tuple'>: ((0, 0.6), (0.2, 1), (0, 0.1))
+    def generate_report(self, pricing_data, weight_bounds, base_asset, frequency: int):
         # print(self.progress('Processing Pricing'))
+        # Transform prices ohlcv data and add 1 price base asset column
+        p_d = deepcopy(self.build_pricing_data_from_ohlcv(pricing_data, base_asset))
+        # L2 regularisation parameter, defaults to 0. Increase if you want more non-negligible weights
+        gamma = 0
         analysis_range_begin, analysis_range_end, ef = self.generate_analysis_model(
-                p_d, weight_bounds, frequency=frequency)
-        # Optimise for maximal Sharpe ratio
-        # Choosing assets
-        _omw = ef.efficient_risk(target_risk=0.01)
+                p_d, weight_bounds, frequency, gamma)
+
+        # Calculate the 'Markowitz portfolio', minimising volatility for a given target_return.
+        # target_return: the desired return of the resulting portfolio
+        _oer = ef.efficient_return(target_return=0.5)
+
+        # Calculate the Sharpe-maximising portfolio for a given volatility(max return for a target_risk).
+        # target_risk: the desired volatility of the resulting portfolio
+        # _omw = ef.efficient_risk(target_risk=0.07)
+
+        # Minimise volatility
         # _raw_weights = ef.min_volatility()
+
+        # Maximise the Sharpe Ratio
+        # _raw_weights = ef.max_sharpe()
+
         cleaned_weights = ef.clean_weights()
         non_zero_weights = dict(filter(lambda w: w[1] > 0.0, cleaned_weights.items()))
-        # Calculating sharpe ratios
+        # Calculating sharpe ratios of above portfolio
         mu, sigma, sharpe = ef.portfolio_performance()
-        # _mu, sigma, sharpe = ef.portfolio_performance(verbose=True)
         # Calculating Returns
         range_begin, range_end, wavg_return = self.forward_looking_return(
             p_d,
@@ -144,8 +156,8 @@ class PortfolioOpt:
         # Prepare list to pass to ui api
         p_list = []
         p_list.append("Period: {} - {}".format(str(range_begin), str(range_end)))
-        p_list.append("Expected return: {:.2f}%".format(100 * mu))
         p_list.append("Portfolio return: {:.2f}%".format(100 * wavg_return))
+        p_list.append("Expected return: {:.2f}%".format(100 * mu))
         p_list.append("Volatility: {:.2f}%".format(100 * sigma))
         p_list.append("Sharpe Ratio: {:.2f}".format(sharpe))
         for key, value in sorted(non_zero_weights.items(), key=lambda kv: -kv[1]):
@@ -154,3 +166,7 @@ class PortfolioOpt:
         return non_zero_weights, p_list
 
     # endregion
+
+    def make_weight_bounds(self, weight_bounds):
+        # TODO Add separate bounds for each asset <class 'tuple'>: ((0, 0.6), (0.2, 1), (0, 0.1))
+        pass
