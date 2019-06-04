d0f1ec5d3e8a9352c8ea5177dfc216496d09573b
Nekrasovp <nekrasovp@gmail.com>
2019-06-04 19:49:54 +0300

    refactor self to exchange

--- yat/scindex.py
+++ yat/scindex.py
@@ -1,20 +1,20 @@
 
-def _calculate_index(self):
+def _calculate_index(exchange):
     overall_volume = 0
     index_ask_price = 0
     index_bid_price = 0
-    for s in self.scindex_markets:
-        overall_volume += self.exchange1.all_tickers[s]['baseVolume']
-    for s in self.scindex_markets:
-        index_ask_price += self.exchange1.all_tickers[s]['ask'] * self.exchange1.all_tickers[s][
+    for s in exchange.scindex_markets:
+        overall_volume += exchange.all_tickers[s]['baseVolume']
+    for s in exchange.scindex_markets:
+        index_ask_price += exchange.all_tickers[s]['ask'] * exchange.all_tickers[s][
             'baseVolume'] / overall_volume
-        index_bid_price += self.exchange1.all_tickers[s]['bid'] * self.exchange1.all_tickers[s][
+        index_bid_price += exchange.all_tickers[s]['bid'] * exchange.all_tickers[s][
             'baseVolume'] / overall_volume
     return overall_volume, index_ask_price, index_bid_price
 
-def _count_sci_balance(self, asset):
+def _count_sci_balance(exchange, asset):
     if asset == 'BTC':
-        btc_balance = self.balances[asset]['all']
+        btc_balance = exchange.balances[asset]['all']
     else:
-        btc_balance = self._count_btc_balance(self.balances[asset]['all'], asset)
-    return btc_balance * (self.scindex_value['ask']+self.scindex_value['bid'])/2
+        btc_balance = exchange._count_btc_balance(exchange.balances[asset]['all'], asset)
+    return btc_balance * (exchange.scindex_value['ask']+exchange.scindex_value['bid'])/2
