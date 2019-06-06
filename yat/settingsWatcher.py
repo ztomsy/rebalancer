5cfee426f60c4283c8d309fd99e2e661781de9e2
Nekrasovp <nekrasovp@gmail.com>
2019-06-06 13:33:24 +0300

    add docstrings

--- yat/settingsWatcher.py
+++ yat/settingsWatcher.py
@@ -5,16 +5,17 @@ import settings
 
 class settingsWatcher(object):
 
-    # Constructor
-    def __init__(self, watch_file: str = 'settings.py'):
+    def __init__(self):
         self._cached_stamp = 0
-        self.settings = {}
+        self.settings = None
         self.settings = self.build_dict()
-        self.filename = watch_file
+        self.filename = self.settings['WATCHED_FILE']
         self.call_func_on_change = self.reload_watched_file
 
-    # Look for changes
     def look(self):
+        """
+        Look for changes
+        """
         is_refreshed = False
         stamp = os.stat(self.filename).st_mtime
         if stamp != self._cached_stamp:
@@ -25,14 +26,16 @@ class settingsWatcher(object):
                 self.call_func_on_change()
         return is_refreshed
 
-    # Call this function each time a change happens
     def reload_watched_file(self):
-        self.importOrReload('settings')
+        """
+        Call this function each time a change happens
+        """
+        self.import_or_reload('settings')
         self.settings.clear()
         self.settings = self.build_dict()
 
     @staticmethod
-    def importOrReload(module_name, *names):
+    def import_or_reload(module_name, *names):
         import sys
 
         if module_name in sys.modules:
@@ -44,7 +47,8 @@ class settingsWatcher(object):
             globals()[name] = getattr(sys.modules[module_name], name)
 
     def build_dict(self):
-        """Build and return a dict of settings from imported module
+        """
+        Build and return a dict of settings from imported module
         """
         return {attr: getattr(settings, attr) for attr in dir(settings) if not callable(
                 getattr(settings, attr)) and not attr.startswith("__")}
