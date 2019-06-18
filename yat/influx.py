dda4f72ed2db43c9126ce3807fcb22ab97f529e7
Nekrasovp <nekrasovp@gmail.com>
2019-06-18 19:31:28 +0300

    add report_order and report_market_state methods

--- yat/influx.py
+++ yat/influx.py
@@ -31,3 +31,36 @@ class Influx(object):
         except Exception as e:
             print('Querying data from InfluxDB failed: ', type(e).__name__, "-=-=-", e.args, '-=-=-', str(e))
 
+    def report_order(self, order: dict, provider: str, exchange: str):
+        """
+        Prepare order dict to supported json format and write it with provided client
+
+        :param order:
+        :param provider:
+        :param exchange:
+        :return:
+        """
+        prepared_order = dict()
+        prepared_order['measurement'] = 'orders_data'
+        prepared_order['tags'] = {'provider': provider,
+                                  'exchange': exchange}
+        prepared_order['fields'] = dict(id=order['id'],
+                                        timestamp=order['timestamp'],
+                                        symbol=order['symbol'],
+                                        type=order['type'],
+                                        side=order['side'],
+                                        price=order['price'],
+                                        amount=order['filled'],
+                                        status=order['status'],
+                                        fee=order['fee'],)
+        self._write_points([prepared_order, ])
+
+    def report_market_state(self, market_state: dict):
+        p_state = dict()
+        p_state['measurement'] = 'market_state_data'
+        p_state['tags'] = {'ticker': market_state['ticker'],
+                           'exchange': market_state['exchange']}
+        p_state['fields'] = dict(param1=market_state['param1'],
+                                 param2=market_state['param2'],)
+        self._write_points(p_state)
+        pass
